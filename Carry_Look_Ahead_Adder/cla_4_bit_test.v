52ea6bc01cdd718b23be0257f59c537b2645343c
7enTropy7 <menon.uk1998@gmail.com>
2020-01-15 12:47:52 +0530

    Testbench code added

---
+++ Carry_Look_Ahead_Adder/cla_4_bit_test.v
@@ -0,0 +1,17 @@
+`timescale 1ns / 1ps
+
+module cla_4_bit_test();
+    reg [3:0]a,b;
+    reg cin;
+    wire [4:0]s;
+    reg [4:0]check;
+    cla_4_bit uut(a,b,cin,s);
+    initial repeat (10) begin
+        a = $random;
+        b = $random;
+        cin = $random;
+        check = a+b+cin;
+        #10 $display($time," %d + %d + %d = %d (%d) ",a,b,cin,s,check);
+     end
+ endmodule
+ 
